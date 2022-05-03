a6a4dea6ba14060465774910f0d87e65802384c5
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-05-03 14:06:07 -0300

    feat: get redirect url from headers only

--- forward_auth/auth_server/utils.py
+++ forward_auth/auth_server/utils.py
@@ -10,7 +10,7 @@ def get_public_uri() -> str:
     Returns:
         The public URI.
     """
-    return getenv('DJANGO_AUTH_PUBLIC_URI', '')
+    return getenv("DJANGO_AUTH_PUBLIC_URI", "")
 
 
 def get_redirect_uri(request: HttpRequest, default: str = None) -> str:
@@ -25,26 +25,17 @@ def get_redirect_uri(request: HttpRequest, default: str = None) -> str:
         The redirect_uri from the request.
     """
     # First we try to get the redirect_uri from the request.
-    redirect_uri = request.GET.get('redirect', None)
+    redirect_uri = request.headers.get("X-Original-Url")
 
     # If the redirect_uri is not in the request, we try to get it
     # from headers.
-    if (
-        (redirect_uri is None)
-        and
-        ("X-Forwarded-Host" in request.headers)
-    ):
+    if (redirect_uri is None) and ("X-Forwarded-Host" in request.headers):
         redirect_uri = f"{request.headers['X-Forwarded-Proto']}://{request.headers['X-Forwarded-Host']}"
 
     # If the redirect_uri is still not in the request, we try to get it
     # from the default value.
     if redirect_uri is None:
         redirect_uri = default
-
-    # If the redirect_uri is still not in the request, we return an empty
-    # string.
-    if redirect_uri is None:
-        redirect_uri = ""
 
     # We return the redirect_uri.
     return redirect_uri
--- forward_auth/auth_server/views.py
+++ forward_auth/auth_server/views.py
@@ -11,28 +11,24 @@ from .utils import (
 def auth(request: HttpRequest):
     if request.user.is_authenticated:
         return HttpResponse("OK")
-    else:
-        redirect_uri = get_redirect_uri(request)
-        if redirect_uri:
-            return redirect(f"{get_public_uri()}/auth/login/?redirect={redirect_uri}")
-        return redirect(f"{get_public_uri()}/auth/login/")
+    return HttpResponse("Unauthorized", status=401)
 
 
 def signin(request: HttpRequest):
-    redirect_uri = get_redirect_uri(request, default="/")
+    redirect_uri = get_redirect_uri(request)
     if request.user.is_authenticated:
         return redirect(redirect_uri)
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             response: HttpResponse = redirect(redirect_uri)
             return response
         else:
-            return HttpResponse('Invalid login')
-    return render(request, 'login.html')
+            return HttpResponse("Invalid login")
+    return render(request, "login.html")
 
 
 def signout(request: HttpRequest):
